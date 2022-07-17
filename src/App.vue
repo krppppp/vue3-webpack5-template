950241c21415621e0c36523f24735309dab1e498
chenuin <yuyin0036@gmail.com>
2022-07-17 15:30:46 +0800

    Apply <script setup>

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,29 @@
   <div class="app">
     <p
       v-text="msg" />
+    <button
+      @click="addClickTimes(count)"
+      v-text="'Click'" />
+    <p
+      v-text="`Click ${pluralizeCount}`" />
   </div>
 </template>
 
-<script>
-export default {
-    name: 'App',
-    setup() {
-      return {
-        msg: 'Hello World',
-      };
-    },
-};
+<script setup>
+import { ref, computed } from 'vue'
+
+const msg = 'Hello World!';
+const count = ref(0);
+
+const pluralizeCount = computed(() => pluralize('time', count));
+
+function pluralize(word, count) {
+  const countValue = count.value;
+
+  return `${countValue} ${word}${countValue <= 1 ? '' : 's'}`;
+}
+
+function addClickTimes(value) {
+  count.value = value + 1
+}
 </script>
