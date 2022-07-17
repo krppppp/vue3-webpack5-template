b85576a06888ed9bc9ea9c08477ec2f9c8782eb9
chenuin <yuyin0036@gmail.com>
2022-07-17 20:37:01 +0800

    chore: Add components to webpack resolve alias

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
     },
     resolve: {
         alias: {
+            components: path.resolve(__dirname, 'src/components/'),
             vue: 'vue/dist/vue.esm-bundler.js',
         },
     },
